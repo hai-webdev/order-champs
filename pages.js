6313a3ee6f84380bf3ed3d393f8ab4f98216dcfa
zb_hai <hai_web@163.com>
2023-02-05 15:46:58 +0800

    初始提交

---
+++ pages.js
@@ -0,0 +1,27 @@
+module.exports = {
+  index: {
+    js: "./src/pages/index",
+    html: "./src/pages/index/index.html",
+    out: "index.html",
+  },
+  about: {
+    js: "./src/pages/about",
+    html: "./src/pages/about/index.html",
+    out: "about.html",
+  },
+  contact: {
+    js: "./src/pages/contact",
+    html: "./src/pages/contact/index.html",
+    out: "contact.html",
+  },
+  product: {
+    js: "./src/pages/product",
+    html: "./src/pages/product/index.html",
+    out: "product.html",
+  },
+  product_view: {
+    js: "./src/pages/product_view",
+    html: "./src/pages/product_view/index.html",
+    out: "product_view.html",
+  },
+};
---
+++ src/pages/common/crumbs/index.js
@@ -0,0 +1,4 @@
+// 头部的js代码
+import "./index.less";
+import "swiper/css/swiper.min.css";
+import "wowjs/css/libs/animate.css";
